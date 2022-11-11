3412951c01e4d4ceaf13471fbbdc0484c73d5cac
Javier <javcaracuel1@gmail.com>
2022-11-11 18:48:31 +0100

    nuevos archivos y funciones para conseguir los datos de la api

---
+++ src/kraken_functions.py
@@ -0,0 +1,20 @@
+from .libraries import krakenex
+from .libraries import KrakenAPI
+
+def get_cripto_pairs(tipo):
+    api = krakenex.API()
+    k = KrakenAPI(api)
+    aux=k.get_tradable_asset_pairs()
+    aux=aux[aux["altname"].str.contains(tipo,case=False)]
+    aux=aux["altname"]
+    lista_aux=aux.to_list()
+    lista=[]
+    for i,j in enumerate(lista_aux):
+        lista.append((j,i))
+    return lista
+
+def recoger_datos(moneda):
+    api = krakenex.API()
+    k = KrakenAPI(api)
+    ohlc, last = k.get_ohlc_data(moneda)
+    return ohlc, last
---
+++ src/libraries.py
@@ -0,0 +1,4 @@
+import krakenex
+from pykrakenapi import KrakenAPI as KrakenAPI
+import ipywidgets as widgets
+import pandas as pd
