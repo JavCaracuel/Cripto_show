44bcbcf984ab29966ce4f49e009324459aed290b
Javier <javcaracuel1@gmail.com>
2022-12-27 16:57:32 +0100

    cambios para el setup

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(
     description='Usefull library for web scrapping cripto currencys value and plotting with metrics on streamlit.',
     author='Enrique and Javier',
     author_email='pepeelhackeos@gmail.com',
+    url='https://github.com/JavCaracuel/Cripto_show'
 )
